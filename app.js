580137ab88df9689af3c637e675a62513830ac4c
unknown <anshuman2121@gmail.com>
2019-08-31 16:18:39 +0530

    V1 Completed

--- app.js
+++ app.js
@@ -1,19 +1,35 @@
 var express = require("express");
 var app = express();
+var bodyparser = require("body-parser");
+app.use(bodyparser.urlencoded({extended: true}));
 app.set("view engine","ejs");
+var campgrounds = [
+    {name:"Mountains Camp", image:"https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
+    {name:"Forest", image:"https://images.unsplash.com/photo-1537565266759-34bbc16be345?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
+    {name:"River Side Camp", image:"https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"}
+];
 
 app.get("/", function(req, res){
-    res.render("landing")
+    res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res){
-    var campgrounds = [
-        {name:"Mountains Camp", image:"https://images.unsplash.com/photo-1505735754789-3404132203ed?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
-        {name:"Breaking Bad Camp", image:"https://images.unsplash.com/photo-1523987355523-c7b5b0dd90a7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"},
-        {name:"River Side Camp", image:"https://images.unsplash.com/photo-1519095614420-850b5671ac7f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1000&q=60"}
-    ]
     res.render("campgrounds",{campgrounds:campgrounds});
 });
+
+app.post("/campgrounds", function(req, res){
+    //get data from form and add to campgrounds
+    var name = req.body.name;
+    var image = req.body.image;
+    var newcamp = {name:name, image:image};
+    campgrounds.push(newcamp);
+    //redirrct to camp ground page
+    res.redirect("/campgrounds");
+} );
+
+app.get("/campgrounds/new", function (req,res) {
+    res.render("new");
+  });
 
 app.listen(80,function(){
     console.log("Camp Website has started");
